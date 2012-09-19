2ffbf8be6c32c232a1baa6cb5d2a6729cb6d4fb0
Lorenzo Beretta <vc.net.loreb@gmail.com>
2012-09-20 00:29:34 +0200

    Set the standard descriptors to binary mode asap!
    I tried porting http://www.acme.com/software/micro_httpd/
    to get an idea of potential probelms I could find.
    Well, port it, test it from the command line (it's an inetd program),
    write a mini-inetd script, launch $BROWSER -- fail.
    
    It took me some time to figure out.

--- src/lib9/main.c
+++ src/lib9/main.c
@@ -7,6 +7,11 @@ extern void p9main(int, char**);
 int
 main(int argc, char **argv)
 {
+	/* Binary mode on standard descriptors -- now! */
+	int fd;
+	for(fd = 0; fd <= 2; fd++)
+		if(setmode(fd, O_BINARY) < 0)
+			sysfatal("binmode(%d): %r", fd);
 	p9main(argc, argv);
 	exits("main");
 	return 99;
